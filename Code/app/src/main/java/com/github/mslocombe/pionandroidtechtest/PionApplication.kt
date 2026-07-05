857ea0841c4d439d0210aa952d45c4fde4f26e50
mSlocombe <slocombe90@gmail.com>
2026-07-05 14:14:45 +0100

    Photos Screen
    - Coil added to the project for AsyncImage loading

--- Code/app/src/main/java/com/github/mslocombe/pionandroidtechtest/PionApplication.kt
+++ Code/app/src/main/java/com/github/mslocombe/pionandroidtechtest/PionApplication.kt
@@ -1,7 +1,19 @@
 package com.github.mslocombe.pionandroidtechtest
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.network.ktor3.KtorNetworkFetcherFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PionApplication : Application()
+class PionApplication : Application(), SingletonImageLoader.Factory {
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .components {
+                add(KtorNetworkFetcherFactory())
+            }
+            .build()
+    }
+}
