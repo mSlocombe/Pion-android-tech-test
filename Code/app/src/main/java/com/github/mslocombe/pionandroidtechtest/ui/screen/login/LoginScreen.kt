07f87af0d7a92bfd69971aa79c6558706a6eb750
mSlocombe <slocombe90@gmail.com>
2026-07-05 20:40:42 +0100

    Photos Screen
    - Preview fixes

--- Code/app/src/main/java/com/github/mslocombe/pionandroidtechtest/ui/screen/login/LoginScreen.kt
+++ Code/app/src/main/java/com/github/mslocombe/pionandroidtechtest/ui/screen/login/LoginScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -137,6 +138,9 @@ private fun PasswordField(
 @Composable
 private fun Preview_LoginScreen() {
     SBTechincalTestTheme {
-        LoginScreen() {}
+        val viewModel = remember { LoginViewModelImpl() }
+        LoginScreen(
+            viewModel
+        ) {}
     }
 }
--- Code/app/src/main/java/com/github/mslocombe/pionandroidtechtest/ui/screen/photos/content/ReadyContent.kt
+++ Code/app/src/main/java/com/github/mslocombe/pionandroidtechtest/ui/screen/photos/content/ReadyContent.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.mslocombe.pionandroidtechtest.ui.screen.photos.PhotoCard
 import com.github.mslocombe.pionandroidtechtest.ui.screen.photos.PhotoCardState
+import kotlin.random.Random
 
 @Composable
 fun ReadyContent(
@@ -29,4 +32,16 @@ fun ReadyContent(
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun Preview_ReadyContent() {
+    ReadyContent(
+        cards = listOf(
+            PhotoCardState(1, "Title #1", "", Color(Random.nextInt())),
+            PhotoCardState(2, "Title #2", "", Color(Random.nextInt())),
+            PhotoCardState(3, "Title #3", "", Color(Random.nextInt()))
+        )
+    )
 }
